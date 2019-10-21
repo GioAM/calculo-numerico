52b3fd06ad55e572e240064e46e6983fe4a4ecec
giovani.meneguel <gio_meneguel@hotmail.com>
2019-10-21 18:18:53 -0300

    Adicionando informações e calculo de secantes

--- js/custom.js
+++ js/custom.js
@@ -6,6 +6,12 @@ function Iteracao(id, A, B, X, Fx, taxaDeErro) {
   this.Fx = Fx;
   this.taxaDeErro = taxaDeErro;
 }
+function IteracaoSec(id, X, Fx, taxaDeErro) {
+  this.id = id;
+  this.X = X;
+  this.Fx = Fx;
+  this.taxaDeErro = taxaDeErro;
+}
 $('#bissecao').click(function(){
   bissecao();
 });
@@ -15,7 +21,11 @@ function calcularMaxIteracoes(A, B, errorTax){
   return parseInt(total,10)
 }
 function funcao(value){
-  var total = Math.pow(value, 2) - 2;
+  var scope = {
+    x: value
+  }
+  var funcao = $("#function").val();
+  var total = math.evaluate(funcao, scope);
   return parseFloat(total.toFixed(4));
 }
 function calcularPontoMedio(A,B){
@@ -37,8 +47,8 @@ function drawTable(iteracoes){
 }
 function bissecao(){
   var iteracoes = [];
-  var A = parseInt($('#valueA').val(),10);
-  var B = parseInt($('#valueB').val(),10);
+  var A = parseFloat($('#valueA').val());
+  var B = parseFloat($('#valueB').val());
   var X = calcularPontoMedio(A,B);
   var Fx = funcao(X);
   var taxErro = $('#errorTax').val();
@@ -59,4 +69,59 @@ function bissecao(){
   }
   drawTable(iteracoes);
   console.log(iteracoes);
+}
+$('#secantes').click(function(){
+  secantes();
+});
+function secantes(){
+  var iteracoes = [];
+  var A = parseFloat($('#valueASec').val());
+  var B = parseFloat($('#valueBSec').val());
+  var taxErro = $('#errorTaxSec').val();
+  var id = 2;
+
+  var Fa = funcaoSec(A);
+  var Fb = funcaoSec(B);
+
+  var iteracao = new IteracaoSec(0, A, Fa.toFixed(4), "-");
+  iteracoes.push(iteracao);
+  var iteracao = new IteracaoSec(1, B, Fb.toFixed(4), Math.abs(A - B));
+  iteracoes.push(iteracao);
+  var erro = 1;
+  while(erro > taxErro){
+    iteracoes.push(calcularSecantes(iteracoes, id));
+    erro = iteracoes[id].taxaDeErro;
+    id++;
+  }
+  console.log(iteracoes);
+  drawTableSec(iteracoes);
+}
+function calcularSecantes(iteracoes, id){
+  var scope = {
+    a: iteracoes[id-2].X,
+    b: iteracoes[id-1].X,
+    c: iteracoes[id-2].Fx,
+    d: iteracoes[id-1].Fx
+  }
+  var total = math.evaluate("((a*d)-(b*c))/(d-c)", scope);
+  var Ftotal = funcaoSec(total);
+  var iteracao = new IteracaoSec(id, total.toFixed(4), Ftotal.toFixed(4), Math.abs(scope.b - total).toFixed(4));
+  return iteracao;
+}
+function funcaoSec(value){
+  var scope = {
+    x: value
+  }
+  var funcao = $("#functionSec").val();
+  var total = math.evaluate(funcao, scope);
+  return parseFloat(total.toFixed(4));
+}
+function drawTableSec(iteracoes){
+  $("#table-secantes").empty();
+  var taxErro = $('#errorTaxSec').val();
+  $('#table-secantes').append(`<thead id='thead-secantes'><tr><th scope='col'>n</th><th scope='col'>Xn</th><th scope='col'>f(Xn)</th><th scope='col'>Taxa de Erro(${taxErro})</th></tr></thead><tbody id='tbody-secantes'></tbody>`);
+   for(var i = 0; i < iteracoes.length; i++){
+     var element = iteracoes[i];
+     $('#tbody-secantes').append(`<tr><th>${element.id}</th><th>${element.X}</th><th>${element.Fx}</th><th>${element.taxaDeErro}</th></tr>`)
+   }
 }
